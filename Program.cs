da798d5cad088fcadba65ce87dd856aab7dab66b
Mohammad Jabir <mohammadj94@gmail.com>
2023-01-12 21:05:11 -0700

    level 5 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Quack!");
+﻿//level 3
+string name;
+Console.WriteLine("Bread is ready./nWho is the bread for?");
+name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got bread.");
