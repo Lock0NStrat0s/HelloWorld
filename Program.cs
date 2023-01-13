19117350bb78dec4539cc6bf2c4267dc115df71a
Mohammad Jabir <mohammadj94@gmail.com>
2023-01-13 06:39:21 -0700

    level 7

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
 ﻿//level 3
-string name;
-Console.WriteLine("Bread is ready./nWho is the bread for?");
-name = Console.ReadLine();
-Console.WriteLine("Noted: " + name + " got bread.");
+//string name;
+//Console.WriteLine("Bread is ready./nWho is the bread for?");
+//name = Console.ReadLine();
+//Console.WriteLine("Noted: " + name + " got bread.");
+
+//level 6 
+//the variable shop
+byte b = 12;                //byte
+//Console.WriteLine(b);
+//short
+//int
+//long
+//sbyte
+//ushort
+//uint
+ulong num = 1_000_000_000_000_000UL;      //ulong
+Console.WriteLine(num);
